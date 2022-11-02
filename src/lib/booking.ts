c3fd32795dc95ccafcd8ab306cf3beb57c7254e7
Christopher Wittor <chris@wittor.it>
2022-11-02 20:47:19 +0100

    Added type Invoices and Bookings

---
+++ src/lib/booking.ts
@@ -0,0 +1,37 @@
+import {
+  createParameterizedApiRoute,
+  performPagedRequest,
+  performRequest,
+} from '.';
+import { Booking } from '../types/Booking';
+
+/**
+ * Returns the bookings with the given ID
+ * @param id - The id of the invoice
+ * @param query - String containing fields to query for, e.g. '{id,amount,relatedInvoice{id,totalPrice}}'. It is highly recommended to use this to make the response as small as possible.
+ * @returns A Booking object.
+ */
+export const getBooking = (
+  id: number,
+  query?: string
+): Promise<Booking> =>
+  performRequest(
+    createParameterizedApiRoute(`/booking/${id}/`, {
+      query: query,
+    })
+  );
+
+/**
+ * Return all bookings by fetching all pages
+ * @param query -  String containing fields to query for, e.g. '{id,amount,relatedInvoice{id,totalPrice}}'. It is highly recommended to use this to make the response as small as possible.
+ * @returns An array of Booking objects.
+ */
+export const getBookings = (
+  query?: string
+): Promise<Booking[]> =>
+  performPagedRequest(
+    createParameterizedApiRoute('/booking/', {
+      query: query,
+    }),
+    []
+  );
--- src/lib/index.spec.ts
+++ src/lib/index.spec.ts
@@ -4,11 +4,15 @@ import { getMember, getMembers, updateMember } from './member';
 import { getOrganization } from './organization';
 
 import { setApiToken } from '.';
+import { getInvoice, getInvoices } from './invoice';
+import { getBooking, getBookings } from './booking';
+
+// ToDo: Should be splitted up in seperate files to proper test (e.g. getting a valid memberId before the single member tests run, or cleanup the changes made to a member)
 
 console.log(`Running tests with API token: ${process.env.EASYVEREIN_TOKEN}`);
 
 test('we can set token', (t) => {
-  t.notThrows(() => setApiToken(process.env.EASYVEREIN_TOKEN))
+  t.notThrows(() => setApiToken(process.env.EASYVEREIN_TOKEN));
 });
 
 test('we can fetch the organization', async (t) => {
@@ -27,7 +31,7 @@ test('we can fetch the organization with only the short name', async (t) => {
 test('we can fetch all members', async (t) => {
   const members = await getMembers();
   // Every member contains the email field and a lot of other fields
-  members.map(m => {
+  members.map((m) => {
     t.truthy(m.id);
     t.truthy(m.email);
   });
@@ -37,17 +41,19 @@ test('we can fetch all members', async (t) => {
 test('we can fetch all members with only the id', async (t) => {
   const members = await getMembers('{id}');
   // Every member contains the id field, but not the email field
-  members.map(m => {
+  members.map((m) => {
     t.truthy(m.id);
     t.falsy(m.email);
   });
   t.true(members.length > 0);
 });
 
-
 test('we can fetch a single member by id', async (t) => {
-  const id = 414902;
-  const member = await getMember(id, '{id,contactDetails{name, privateEmail},email,membershipNumber,memberGroups}');
+  const id = 1461395;
+  const member = await getMember(
+    id,
+    '{id,contactDetails{name, privateEmail},email,membershipNumber,memberGroups}'
+  );
   t.is<number>(member.id, id);
   t.truthy(member.membershipNumber);
   t.truthy(member.email);
@@ -55,9 +61,73 @@ test('we can fetch a single member by id', async (t) => {
 });
 
 test('we can change the membership number of a single member', async (t) => {
-  const id = 414902;
+  const id = 1461395;
   const randomMembership = (Math.random() * 100).toFixed();
-  const member = await updateMember(id, { membershipNumber: randomMembership })
+  const member = await updateMember(id, { membershipNumber: randomMembership });
   t.is<number>(member.id, id);
   t.is(member.membershipNumber, randomMembership);
+});
+
+test('we can fetch all invoices', async (t) => {
+  const invoices = await getInvoices();
+  // Ever invoice contains an id and a total price
+  invoices.map((inv) => {
+    t.truthy(inv.id);
+    t.truthy(inv.totalPrice);
+  });
+  t.true(invoices.length > 0);
+});
+
+test('we can fetch all invoices with only the id', async (t) => {
+  const invoices = await getInvoices('{id}');
+  // Every invoice contains the id field, but not the total price
+  invoices.map((inv) => {
+    t.truthy(inv.id);
+    t.falsy(inv.totalPrice);
+  });
+  t.true(invoices.length > 0);
+});
+
+test('we can fetch a single invoice by id', async (t) => {
+  const id = 51152175;
+  const invoice = await getInvoice(
+    id,
+    '{id,totalPrice,relatedAddress{id, referencedMemberPK},kind}'
+  );
+  t.is<number>(invoice.id, id);
+  t.truthy(invoice.totalPrice);
+  t.truthy(invoice.relatedAddress.id);
+  t.falsy(invoice.org);
+});
+
+test('we can fetch all bookings', async (t) => {
+  const bookings = await getBookings();
+  // Ever booking contains an id and a total price
+  bookings.map((b) => {
+    t.truthy(b.id);
+    t.truthy(b.amount);
+  });
+  t.true(bookings.length > 0);
+});
+
+test('we can fetch all bookings with only the id', async (t) => {
+  const bookings = await getBookings('{id}');
+  // Every invoice contains the id field, but not the total price
+  bookings.map((b) => {
+    t.truthy(b.id);
+    t.falsy(b.amount);
+  });
+  t.true(bookings.length > 0);
+});
+
+test('we can fetch a single booking by id', async (t) => {
+  const id = 55205784;
+  const booking = await getBooking(
+    id,
+    '{id,amount,relatedInvoice{id,totalPrice},date}'
+  );
+  t.is<number>(booking.id, id);
+  t.truthy(booking.amount);
+  t.truthy(booking.relatedInvoice[0].id);
+  t.falsy(booking.org);
 });

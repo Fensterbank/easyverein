59772d578962ac929ef7aba992d5d0e49026ced2
Christopher Wittor <chris@wittor.it>
2024-01-17 19:36:48 +0100

    New method: getInvoicesBy(any, string) Allows a paginated query of invoices filtered according to defined criteria

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-export { setApiToken } from './lib/';
+export { setApiToken, setApiVersion } from './lib/';
 export {
   countContactDetails,
   getContactDetail,
   getContactDetails,
 } from './lib/contactDetail';
 export { countMembers, getMember, getMembers } from './lib/member';
+export { getInvoice, getInvoices, getInvoicesBy } from './lib/invoice';
 export { getOrganization } from './lib/organization';
 export {
   Collection,
--- src/lib/invoice.ts
+++ src/lib/invoice.ts
@@ -35,3 +35,23 @@ export const getInvoices = (
     }),
     []
   );
+
+/**
+ * Return criteria matched invoices by fetching all pages
+ * @param filter - Object with query criterias for filtering invoices (See: https://easyverein.com/api/v1.6/documentation/#/invoice/get_api_v1_6_invoice)
+ * @param query - String containing fields to query for, e.g. '{id,kind,relatedBookings{id,amount,description}}'. It is highly recommended to use this to make the response as small as possible.
+ * @returns An array of Invoice objects.
+ */
+export const getInvoicesBy = (
+  filter: any,
+  query?: string
+): Promise<Invoice[]> =>
+  performPagedRequest(
+    createParameterizedApiRoute('/invoice/', {
+      ...filter,
+      ...{
+        query: query,
+      },
+    }),
+    []
+  );
